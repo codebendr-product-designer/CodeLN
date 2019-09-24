6f946ae88ea40eeab3ecae85bf0aa5404734aa88
codebendr-product-designer <codebendr@gmail.com>
2019-09-24 19:21:06 +0000

    clean up folder structure

--- Feed/Controllers/TableViewController.swift
+++ Feed/Controllers/TableViewController.swift
@@ -11,53 +11,51 @@ import UIKit
 class TableViewController: UITableViewController {
     
     var photos = [Photos]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let urlString = "https://jsonplaceholder.typicode.com/photos?_limit=10"
-              
-              let url = URL(string: urlString)
-              URLSession.shared.dataTask(with:url!) { (data, response, error) in
-                if error == nil {
-                      do {
-
-                        if let data = data {
-                            self.photos = try JSONDecoder().decode([Photos].self,from:data)
-                            print("data")
-                            DispatchQueue.main.async {
-                         self.tableView.reloadData()
-                            }
-                        } else {
-                            print("error with data")
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with:url!) { (data, response, error) in
+            if error == nil {
+                do {
+                    
+                    if let data = data {
+                        self.photos = try JSONDecoder().decode([Photos].self,from:data)
+                        print("data")
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
                         }
-               
-                         }
-                         catch {
-
-                             print(error)
-                         }
-                      
-                } else {
-                    print("some error happened")
+                    } else {
+                        print("error with data")
+                    }
+                    
                 }
-                  
-              }.resume()
-      
+                catch {
+                    
+                    print(error)
+                }
+                
+            } else {
+                print("some error happened")
+            }
+            
+        }.resume()
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return photos.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
@@ -65,12 +63,12 @@ class TableViewController: UITableViewController {
         return cell
     }
     
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard  = UIStoryboard(name: "Main", bundle: nil)
         let VC = storyboard.instantiateViewController(identifier: "DetailViewController") as! DetailViewController
         VC.photo = photos[indexPath.row]
         self.navigationController?.present(VC, animated: true, completion: nil)
     }
-
+    
 }
